2feb207947ce3b2b49ff58a7f8f05146e7a2ee9f
martin <martinyykwong@gmail.com>
2020-04-28 14:09:33 -0700

    Added line7 and line8 after staging. Added file to stage again. Then commit

--- git_workflow_test_file3.rb
+++ git_workflow_test_file3.rb
@@ -4,3 +4,5 @@
 #what if I now add this line after staging? git reset simply moves file off staging area. Doesn't revert changes.
 #what if I add this line before staging? git restore removes all changes and returns to last commit.
 #I can do whatever I want before staging the file.
+#if I add more changes after staging, these new changes won't get committed.
+#if I want to commit to changes that are added after staging, I need to add file to stage again.
