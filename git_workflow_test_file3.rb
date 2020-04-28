e54a0e4c0a3f9ded68442660834a56f895a938f3
martin <martinyykwong@gmail.com>
2020-04-28 13:45:57 -0700

    Added several test lines

--- git_workflow_test_file3.rb
+++ git_workflow_test_file3.rb
@@ -1,1 +1,4 @@
-#testing to see what happens
+#testing to see what happens if edit before tracking starts
+#testing to see what happens if edit after tracking starts
+#what if I add this line before staging?
+#what if I now add this line after staging? git reset simply moves file off staging area. Doesn't revert changes.
